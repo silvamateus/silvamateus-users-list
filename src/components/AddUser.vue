bc401c8e162b0072c33804e27cc8ef0eed0a34c5
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-02 15:22:26 -0300

    add AddUser component

---
+++ src/components/AddUser.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-row class="card-bg justify-center align-center">
+    <v-card class="card-container">
+      <div class="white--text pl-4 card-head">
+        <v-card-title>Cadastrar Aluno</v-card-title>
+      </div>
+      <v-form>
+        <v-row class="pl-8 pr-8 flex-column">
+          <v-col>
+            <v-text-field
+              label="Nome"
+              class="black--text input-height"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="Email"
+              class="black--text input-height"
+            ></v-text-field>
+          </v-col>
+          <v-col lg="4">
+            <v-text-field
+              label="Idade"
+              class="black--text input-height"
+            ></v-text-field>
+          </v-col>
+          <v-col lg="4">
+            <v-text-field
+              label="Telefone"
+              class="black--text input-height"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row class=" justify-space-between">
+          <v-col class=" pt-16">
+            <v-card-actions>
+              <v-btn @click="hideUserModal($event)" text class="cancel-btn"
+                >Cancelar</v-btn
+              >
+            </v-card-actions>
+          </v-col>
+          <v-col class="pt-16 pl-16 ml-16">
+            <v-card-actions>
+              <v-btn class="white--text pl-10 pr-10 btn-color">Salvar</v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    showAddUser: {
+      type: Boolean
+    }
+  },
+  methods: {
+    hideUserModal() {
+      this.$emit("hideAddUser", !this.showAddUser);
+    }
+  }
+};
+</script>
+
+<style>
+.card-bg {
+  background: gray !important;
+  position: fixed !important;
+  height: 100vh !important;
+  min-width: 100vw !important;
+  left: 0;
+  top: 0;
+  z-index: 5;
+}
+.card-head {
+  background-color: #290d95 !important;
+}
+.card-container {
+  height: 65vh;
+  width: 50vw;
+}
+.input-height {
+  height: 2rem;
+}
+.cancel-btn {
+  color: #341c90 !important;
+}
+.btn-color {
+  background-color: #ff6450 !important;
+}
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-row class="d-flex justify-space-around mt-13 mr-11">
-    <v-col class="d-flex ml-15 mt-6" lg="3" md="3" sm="3">
+  <v-row class="d-flex justify-space-between  mt-13">
+    <v-col class="d-flex ml-15 mt-6" lg="3" md="3">
       <v-text-field
         class="size-1-5rem rounded-0"
         style="border-top-right-radius: .3rem !important; border-bottom-right-radius: .3rem !important;"
@@ -8,18 +8,16 @@
         solo
       />
       <v-btn
+        color="white"
         style="border-top-right-radius: .3rem !important; border-bottom-right-radius: .3rem !important;"
         class="size-1-5rem search-bar rounded-0"
         ><v-icon>mdi-magnify</v-icon></v-btn
       >
     </v-col>
-    <v-spacer></v-spacer>
-    <v-col>
-      <v-row class="d-flex">
-        <v-col class="align-self-end">
-          <v-subheader>Filtros: </v-subheader>
-        </v-col>
-        <v-col>
+    <v-col lg="8" md="6">
+      <v-row class="margin-right-5rem justify-end">
+        <v-subheader class="align-self-center mt-6">Filtros: </v-subheader>
+        <v-col lg="4">
           <v-select
             lg="3"
             md="3"
@@ -28,21 +26,43 @@
             v-model="searchFilter"
             solo
             class="size-1-5rem mt-6 margin-left-n5rem"
+            style="min-width: 7rem !important;"
           ></v-select>
+        </v-col>
+        <v-col class="mr-12 d-flex justify-center" lg="4">
+          <v-btn class="mt-6 mr-16 size-1-5rem add-user-btn white--text"
+            ><v-icon>mdi-account-plus</v-icon>
+            <span
+              @click="showAddUser = !showAddUser"
+              class="ml-1 font-weight-bold"
+              >novo aluno</span
+            >
+          </v-btn>
         </v-col>
       </v-row>
     </v-col>
+    <v-col v-if="showAddUser">
+      <add-user :showAddUser="showAddUser" @hideAddUser="hideModal"
+    /></v-col>
   </v-row>
 </template>
 
 <script>
+import AddUser from "./AddUser.vue";
 export default {
   data() {
     return {
       searchFilter: "todos",
       filters: ["todos"],
-      users: []
+      users: [],
+      showAddUser: false
     };
+  },
+  components: { AddUser },
+  methods: {
+    hideModal(e) {
+      this.showAddUser = e;
+    }
   },
   created() {
     this.users = localStorage.getItem("users");
@@ -50,7 +70,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* custom size */
 .size-1-5rem {
   height: 3rem !important;
@@ -62,13 +82,16 @@ export default {
 /* Search bar style */
 .search-bar {
   right: 8px;
-  background: white !important;
   border: 0 !important;
   box-shadow: 5px 5px 5px -4px rgba(0, 0, 0, 0.2),
     0 6px 5px -5px rgba(0, 0, 0, 0.42) !important;
 }
+/* Add User Style */
+.add-user-btn {
+  background-color: #ff6450 !important;
+}
 /* Apply negative margin */
-.margin-left-n5rem {
-  margin-left: -10rem !important;
+.margin-right-5rem {
+  margin-right: 75px !important;
 }
 </style>
