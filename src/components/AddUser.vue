0b9c36a439215829861c504c1c93c7c214b057ec
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-02 16:07:37 -0300

    add functionality to save user

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -48,9 +48,11 @@
               >
             </v-card-actions>
           </v-col>
-          <v-col class="pt-16 pl-16 ml-16">
+          <v-col lg="4" class="pt-16 pl-16 ml-16">
             <v-card-actions>
-              <v-btn class="white--text pl-10 pr-10 btn-color">Salvar</v-btn>
+              <v-btn @click="saveUser" class="white--text pl-10 pr-10 btn-color"
+                >Salvar</v-btn
+              >
             </v-card-actions>
           </v-col>
         </v-row>
@@ -83,6 +85,25 @@ export default {
   methods: {
     hideUserModal() {
       this.$emit("hideAddUser", !this.showAddUser);
+    },
+    saveUser() {
+      this.$refs.form.validate();
+      const user = {
+        name: this.name,
+        email: this.email,
+        age: this.age,
+        phone: this.phone
+      };
+      let users = [];
+      if (localStorage.getItem("users"))
+        users = JSON.parse(localStorage.getItem("users"));
+      users.push(user);
+      localStorage.setItem("users", JSON.stringify(users));
+      this.name = "";
+      this.email = "";
+      this.age = "";
+      this.phone = "";
+      this.hideUserModal();
     }
   }
 };
@@ -102,11 +123,11 @@ export default {
   background-color: #290d95 !important;
 }
 .card-container {
-  height: 65vh;
+  height: 70vh;
   width: 50vw;
 }
 .input-height {
-  height: 2rem;
+  height: 2.5rem;
 }
 .cancel-btn {
   color: #341c90 !important;
